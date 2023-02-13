aa74ebc474b9f4f606403de13b25601720cea178
eddymandran <eddy.mandran@gmail.com>
2023-02-13 14:58:55 +0100

    new activity

--- app/src/main/java/com/example/kotlincdalyon/MainActivity.kt
+++ app/src/main/java/com/example/kotlincdalyon/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kotlincdalyon
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // add button
+        val button = findViewById<Button>(R.id.start_new_activity_btn)
+        button.setOnClickListener{
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
